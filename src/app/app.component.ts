5d2f676d6e4cd05c089c7e8c27c63a90bd3e584b
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2024-09-13 20:03:02 -0300

    modulos e componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fit-angular-app';
+  title = 'Emilio!';
 }
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  @Input() data!: NavBarComponentData
+}
+
+export interface NavBarComponentData {
+  title: string;
+}
