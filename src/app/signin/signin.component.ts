6a736dc896bd1cfd5fbc979e24a805ab9f380d8a
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2024-09-18 20:28:49 -0300

    diretivas

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,16 +1,23 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SigninService } from './services/SigninService';
+import { OnBlurTriggerValidationDirective } from '../utils/directives/on-blur-trigger-validation.directive';
+import { ListElementsDirective } from '../utils/directives/list-elements.directive';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, OnBlurTriggerValidationDirective, ListElementsDirective],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css',
   providers: [SigninService]
 })
 export class SigninComponent {
+  emailRegExp: RegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  emailErrors: Array<string> = [];
+  phoneRegExp: RegExp = new RegExp(/^(\d{2})(\d{9})$/);
+  phoneErrors: Array<string> = [];
+  
   @Input() email: string = '';
   @Input() phone: string = '';
 
